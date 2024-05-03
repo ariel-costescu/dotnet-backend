8825cb4a2217047b0be8b0cbf0e71b3ab972521b
ariel-costescu <ariel.costescu@gmail.com>
2024-05-03 21:58:35 +0300

    Added capability for multiple concurrent clients to be talking to the same server instance.

--- ServerApp/Server.cs
+++ ServerApp/Server.cs
@@ -1,5 +1,6 @@
 namespace BackendServer;
 
+using System;
 using System.Net;
 using System.Net.WebSockets;
 public class Server
@@ -18,24 +19,55 @@ public class Server
 
         Console.WriteLine($"Server listening on port {serverPort}...");
 
-        var context = await httpListener.GetContextAsync();
-        if (context.Request.IsWebSocketRequest)
+        RegisterHandlers();
+
+        int maxConcurrentRequests = Environment.ProcessorCount;
+        var incoming = new HashSet<Task>();
+        for (int i = 0; i < maxConcurrentRequests; i++)
         {
-            var webSocketContext = await context.AcceptWebSocketAsync(null);
-            var webSocket = webSocketContext.WebSocket;
-
-            RegisterHandlers();
-
-            while (webSocket.State != WebSocketState.CloseReceived)
-            {
-                await HandleWebSocketRequest(webSocket);
-            }
-            await webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Closing normally", CancellationToken.None);
+            incoming.Add(httpListener.GetContextAsync());
         }
-        else
+
+        CancellationTokenSource source = new();
+        CancellationToken token = source.Token;
+
+        while (!token.IsCancellationRequested)
         {
-            context.Response.StatusCode = 400;
-            context.Response.Close();
+            Task t = await Task.WhenAny(incoming);
+            incoming.Remove(t);
+
+            if (t is Task<HttpListenerContext>)
+            {
+                var context = (t as Task<HttpListenerContext>)?.Result;
+                incoming.Add(ProcessRequestAsync(context));
+                incoming.Add(httpListener.GetContextAsync());
+            }
+        }
+
+        Console.WriteLine("Server stopping...");
+        httpListener.Stop();
+    }
+
+    public static async Task ProcessRequestAsync(HttpListenerContext? context)
+    {
+        if (context != null)
+        {
+            if (context.Request.IsWebSocketRequest)
+            {
+                var webSocketContext = await context.AcceptWebSocketAsync(null);
+                var webSocket = webSocketContext.WebSocket;
+
+                while (webSocket.State != WebSocketState.CloseReceived)
+                {
+                    await HandleWebSocketRequest(webSocket);
+                }
+                await webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Closing normally", CancellationToken.None);
+            }
+            else
+            {
+                context.Response.StatusCode = 400;
+                context.Response.Close();
+            }
         }
     }
 
@@ -55,5 +87,5 @@ public class Server
         }
     }
 
-    
+
 }
